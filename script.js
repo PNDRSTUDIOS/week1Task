dc53a4b210556ccf45564a44b746ace6367d05f6
Nithin G V <nithingv336@gmail.com>
2026-07-27 12:19:27 +0530

    1 commit

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-// Store all tasks
-let tasks = [];
 
-// Select HTML elements
+let tasks = [];
+let editingTaskId = null;
+
+
 const taskForm = document.getElementById("task-form");
 const taskTitle = document.getElementById("title");
 const taskDesc = document.getElementById("description");
 const attachmentInput = document.getElementById("image");
 const taskList = document.getElementById("task-list");
+const submitButton = taskForm.querySelector("button[type='submit']");
 
-// Form submit
+
 taskForm.addEventListener("submit", function(event) {
+
+
     event.preventDefault();
 
     const file = attachmentInput.files[0];
+    if (editingTaskId !== null) {
+    const taskToUpdate = tasks.find(t => t.id === editingTaskId);
+
+    taskToUpdate.title = taskTitle.value;
+    taskToUpdate.desc = taskDesc.value;
+    if (file) {
+        taskToUpdate.image = URL.createObjectURL(file);
+    }
+
+    editingTaskId = null;
+    submitButton.textContent = "Add Task";
+
+    renderTasks();
+    taskForm.reset();
+    return;
+}
 
     const newTask = {
         id: Date.now(),
@@ -28,7 +48,7 @@ taskForm.addEventListener("submit", function(event) {
     taskForm.reset();
 });
 
-// Render task cards
+
 function renderTasks() {
 
     taskList.innerHTML = "";
@@ -42,12 +62,34 @@ function renderTasks() {
                         <h3>${task.title}</h3>
                         <p>${task.desc}</p>
                     </div>
+                    <div class="right-side">
 
                     ${task.image ? `<img src="${task.image}" class="task-image">` : ""}
-                </div>
+                <div class="button-group">
+                <button data-id="${task.id}" onclick="editTask(event)">Edit</button>
+                <button data-id="${task.id}" onclick="deleteTask(event)">Delete</button>
+            </div>
             </div>
         `);
 
     });
 
 }
+
+const deleteTask = (event) => {
+    const taskId = Number(event.target.dataset.id);
+    tasks = tasks.filter(task => task.id !== taskId);
+    renderTasks();
+}
+const editTask = (event) => {
+    const taskId = Number(event.target.dataset.id);
+
+    const taskToEdit = tasks.find(t => t.id === taskId);
+
+    taskTitle.value = taskToEdit.title;
+    taskDesc.value = taskToEdit.desc;
+
+    editingTaskId = taskId;
+
+    submitButton.textContent = "Update Task";
+};
